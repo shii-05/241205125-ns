36e2256ec742c3f1512f9334b2b4d02e2c4446b3
内藤　汐梨 <241205125@ccmailg.meijo-u.ac.jp>
2025-05-19 15:14:01 +0900

    blue
    青に変更

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'skyblue', 'lavender', 'lavender', 'teal', 'tan', 'silver', 'pink'
+    'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue'
 ];
 
 // creates a new 4x4 shape in global variable 'current'

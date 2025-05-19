1c8f165800a4455f5d6532a933c308d3b0ef9b7b
内藤　汐梨 <241205125@ccmailg.meijo-u.ac.jp>
2025-05-19 14:16:58 +0900

    変更
    色

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'cyan', 'orange', 'blue', 'yellow', 'red', 'lime', 'purple'
+    'plum', 'lavender', 'skyblue', 'teal', 'ivory', 'silver', 'pink'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
